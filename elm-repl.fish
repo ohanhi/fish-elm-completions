79d7738a6f4e5d3b337d63c7979a6fc6587888c3
T.S. Foster <hi@tsf.io>
2018-01-27 13:37:37 +0000

    Add completions for `elm repl`

--- elm-repl.fish
+++ elm-repl.fish
@@ -1,5 +1,13 @@
-complete -f -c elm-repl -s c -l compiler -a FILE -d 'Provide a path to a specific version of elm-make.'
-complete -f -c elm-repl -s i -l interpreter -a node -d 'Provide a path to a specific JavaScript interpreter'
-complete -f -c elm-repl -s h -l help -d 'Display help message'
-complete -f -c elm-repl -s v -l version -d 'Print version information'
-complete -f -c elm-repl -l 'numeric-version' -d 'Print just the version number'
+function __fish_elm_space_or_hyphen_repl
+    set cmd (commandline -opc)
+
+    [ "$cmd[1]" =  'elm-repl' ]; or [ "$cmd[1..2]" =  'elm repl' ]; and return 0
+
+    return 1
+end
+
+complete -fc elm -c elm-repl -n '__fish_elm_space_or_hyphen_repl' -s c -l compiler -a FILE -d 'Provide a path to a specific version of elm-make.'
+complete -fc elm -c elm-repl -n '__fish_elm_space_or_hyphen_repl' -s i -l interpreter -a node -d 'Provide a path to a specific JavaScript interpreter'
+complete -fc elm -c elm-repl -n '__fish_elm_space_or_hyphen_repl' -s h -l help -d 'Display help message'
+complete -fc elm -c elm-repl -n '__fish_elm_space_or_hyphen_repl' -s v -l version -d 'Print version information'
+complete -fc elm -c elm-repl -n '__fish_elm_space_or_hyphen_repl' -l 'numeric-version' -d 'Print just the version number'
