0f2e3648095a27f4167be3ea273b1d4ad65afc43
T.S. Foster <hi@tsf.io>
2018-01-27 13:35:44 +0000

    Add completions for `elm reactor`

--- elm-reactor.fish
+++ elm-reactor.fish
@@ -1,5 +1,14 @@
-complete -f -c elm-reactor -s a -l address -a 0.0.0.0 -d 'set the address of the server'
-complete -f -c elm-reactor -s p -l port -a 8000 -d 'set the port of the reactor'
-complete -f -c elm-reactor -s h -l help -d 'Display help message'
-complete -f -c elm-reactor -s v -l version -d 'Print version information'
-complete -f -c elm-reactor -l 'numeric-version' -d 'Print just the version number'
+function __fish_elm_space_or_hyphen_reactor
+    set cmd (commandline -opc)
+
+    [ "$cmd[1]" =  'elm-reactor' ]; or [ "$cmd[1..2]" =  'elm reactor' ]; and return 0
+
+    return 1
+end
+
+
+complete -fc elm -c elm-reactor -n '__fish_elm_space_or_hyphen_reactor' -s a -l address -a 0.0.0.0 -d 'set the address of the server'
+complete -fc elm -c elm-reactor -n '__fish_elm_space_or_hyphen_reactor' -s p -l port -a 8000 -d 'set the port of the reactor'
+complete -fc elm -c elm-reactor -n '__fish_elm_space_or_hyphen_reactor' -s h -l help -d 'Display help message'
+complete -fc elm -c elm-reactor -n '__fish_elm_space_or_hyphen_reactor' -s v -l version -d 'Print version information'
+complete -fc elm -c elm-reactor -n '__fish_elm_space_or_hyphen_reactor' -l 'numeric-version' -d 'Print just the version number'
