2f7c95b0cdaa7ce240b0c0606f6e97dbecef9e46
T.S. Foster <hi@tsf.io>
2018-01-27 13:32:58 +0000

    Add completions for `elm make`
    * Also adds file completion to `elm-make` for relevant commands

--- elm-make.fish
+++ elm-make.fish
@@ -1,7 +1,15 @@
-complete -f -c elm-make -l output -a FILE -d 'Write result to the given .html or .js FILE.'
-complete -f -c elm-make -l report -a json -d 'Format of error and warning reports (e.g. --report=json)'
-complete -f -c elm-make -l docs -a docs.json -d 'Write documentation to FILE as JSON.'
-complete -f -c elm-make -l yes -d "Reply 'yes' to all automated prompts."
-complete -f -c elm-make -l debug -d 'Generate programs in debug mode.'
-complete -f -c elm-make -l warn -d 'Report warnings to improve code quality.'
-complete -f -c elm-make -s h -l help -d 'Display help message'
+function __fish_elm_space_or_hyphen_make
+    set cmd (commandline -opc)
+
+    [ "$cmd[1]" =  'elm-make' ]; or [ "$cmd[1..2]" =  'elm make' ]; and return 0
+
+    return 1
+end
+
+complete -c elm -c elm-make -n '__fish_elm_space_or_hyphen_make' -l output -d 'Write result to the given .html or .js FILE.'
+complete -fc elm -c elm-make -n '__fish_elm_space_or_hyphen_make' -l report -a json -d 'Format of error and warning reports (e.g. --report=json)'
+complete -c elm -c elm-make -n '__fish_elm_space_or_hyphen_make' -l docs -a docs.json -d 'Write documentation to FILE as JSON.'
+complete -fc elm -c elm-make -n '__fish_elm_space_or_hyphen_make' -l yes -d "Reply 'yes' to all automated prompts."
+complete -c elm -c elm-make -n '__fish_elm_space_or_hyphen_make' -l debug -d 'Generate programs in debug mode.'
+complete -c elm -c elm-make -n '__fish_elm_space_or_hyphen_make' -l warn -d 'Report warnings to improve code quality.'
+complete -fc elm -c elm-make -n '__fish_elm_space_or_hyphen_make' -s h -l help -d 'Display help message'
